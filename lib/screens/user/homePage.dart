a705357439914f2ad26d682e817154da7d864a34
ahmedsami <ah93sam@gmail.com>
2020-08-10 15:10:47 +0300

    first commit

---
+++ lib/screens/user/homePage.dart
@@ -0,0 +1,456 @@
+import 'package:fashionstore/constants.dart';
+import 'package:fashionstore/models/product.dart';
+import 'package:fashionstore/screens/login_screen.dart';
+import 'package:fashionstore/screens/user/CartScreen.dart';
+import 'package:fashionstore/screens/user/productInfo.dart';
+import 'package:fashionstore/services/store.dart';
+import 'package:fashionstore/services/auth.dart';
+import 'package:fashionstore/widgets/productView.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../functions.dart';
+
+class HomePage extends StatefulWidget {
+  static String id = 'HomePage';
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final auth = Auth();
+  FirebaseUser _loggedUser;
+  int _tabBarIndex = 0;
+  int _bottomBarIndex = 0;
+  final _store = Store();
+  List<Product> _products;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              unselectedItemColor: kUnActiveColor,
+              currentIndex: _bottomBarIndex,
+              fixedColor: kMainColor,
+              onTap: (value) async {
+                if (value == 2) {
+                  SharedPreferences pref =
+                      await SharedPreferences.getInstance();
+                  pref.clear();
+                  await auth.signOut();
+                  Navigator.popAndPushNamed(context, LoginScreen.id);
+                }
+                setState(() {
+                  _bottomBarIndex = value;
+                });
+              },
+              items: [
+                BottomNavigationBarItem(
+                    title: Text('Account'), icon: Icon(Icons.person,color: Colors.blue)),
+                BottomNavigationBarItem(
+                    title: Text('Home'), icon: Icon(Icons.home,color: Colors.green)),
+                BottomNavigationBarItem(  
+                    title: Text('Sign Out'), icon: Icon(Icons.close,color: Colors.red)),
+              ],
+            ),
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              elevation: 0,
+              bottom: TabBar(
+                indicatorColor: kMainColor,
+                onTap: (value) {
+                  setState(() {
+                    _tabBarIndex = value;
+                  });
+                },
+                tabs: <Widget>[
+                  Text(
+                    'Jackets',
+                    style: TextStyle(
+                      color: _tabBarIndex == 0 ? Colors.black : kUnActiveColor,
+                      fontSize: _tabBarIndex == 0 ? 16 : null,
+                    ),
+                  ),
+                  Text(
+                    'Trouser',
+                    style: TextStyle(
+                      color: _tabBarIndex == 1 ? Colors.black : kUnActiveColor,
+                      fontSize: _tabBarIndex == 1 ? 16 : null,
+                    ),
+                  ),
+                  Text(
+                    'T-shirts',
+                    style: TextStyle(
+                      color: _tabBarIndex == 2 ? Colors.black : kUnActiveColor,
+                      fontSize: _tabBarIndex == 2 ? 16 : null,
+                    ),
+                  ),
+                  Text(
+                    'Shoes',
+                    style: TextStyle(
+                      color: _tabBarIndex == 3 ? Colors.black : kUnActiveColor,
+                      fontSize: _tabBarIndex == 3 ? 16 : null,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: <Widget>[
+                jacketView(),
+                ProductsView(kTrousers, _products),
+                ProductsView(kTshirts, _products),
+                ProductsView(kShoes, _products),
+
+              ],
+            ),
+          ),
+        ),
+        Material(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(20, 30, 20, 0),
+            child: Container(
+              height: MediaQuery.of(context).size.height * .1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Stylish'.toUpperCase(),
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,color: Colors.green),
+                  ),
+                  GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, CartScreen.id);
+                      },
+                      child: Icon(Icons.shopping_cart,color: Colors.green,))
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  void initState() {
+    getCurrenUser();
+  }
+
+  getCurrenUser() async {
+    _loggedUser = await auth.getUser();
+  }
+
+  Widget jacketView() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _store.loadProducts(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<Product> products = [];
+          for (var doc in snapshot.data.documents) {
+            var data = doc.data;
+            products.add(Product(
+                pId: doc.documentID,
+                pPrice: data[kProductPrice],
+                pName: data[kProductName],
+                pDescription: data[kProductDescription],
+                pLocation: data[kProductLocation],
+                pCategory: data[kProductCategory]));
+          }
+          _products = [...products];
+          products.clear();
+          products = getProductByCategory(kJackets, _products);
+          return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: .8,
+            ),
+            itemBuilder: (context, index) => Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, ProductInfo.id,
+                      arguments: products[index]);
+                },
+                child: Stack(
+                  children: <Widget>[
+                    Positioned.fill(
+                      child: Image(
+                        fit: BoxFit.fill,
+                        image: AssetImage(products[index].pLocation),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      child: Opacity(
+                        opacity: .6,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 60,
+                          color: Colors.white,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 5),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  products[index].pName,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text('\$ ${products[index].pPrice}')
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            itemCount: products.length,
+          );
+        } else {
+          return Center(child: Text('Loading...'));
+        }
+      },
+    );
+  }
+  Widget ShoeView() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _store.loadProducts(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<Product> products = [];
+          for (var doc in snapshot.data.documents) {
+            var data = doc.data;
+            products.add(Product(
+                pId: doc.documentID,
+                pPrice: data[kProductPrice],
+                pName: data[kProductName],
+                pDescription: data[kProductDescription],
+                pLocation: data[kProductLocation],
+                pCategory: data[kProductCategory]));
+          }
+          _products = [...products];
+          products.clear();
+          products = getProductByCategory(kTshirts, _products);
+          return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: .8,
+            ),
+            itemBuilder: (context, index) => Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, ProductInfo.id,
+                      arguments: products[index]);
+                },
+                child: Stack(
+                  children: <Widget>[
+                    Positioned.fill(
+                      child: Image(
+                        fit: BoxFit.fill,
+                        image: AssetImage(products[index].pLocation),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      child: Opacity(
+                        opacity: .6,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 60,
+                          color: Colors.white,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 5),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  products[index].pName,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text('\$ ${products[index].pPrice}')
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            itemCount: products.length,
+          );
+        } else {
+          return Center(child: Text('Loading...'));
+        }
+      },
+    );
+  }
+  Widget TrousertView() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _store.loadProducts(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<Product> products = [];
+          for (var doc in snapshot.data.documents) {
+            var data = doc.data;
+            products.add(Product(
+                pId: doc.documentID,
+                pPrice: data[kProductPrice],
+                pName: data[kProductName],
+                pDescription: data[kProductDescription],
+                pLocation: data[kProductLocation],
+                pCategory: data[kProductCategory]));
+          }
+          _products = [...products];
+          products.clear();
+          products = getProductByCategory(kTrousers, _products);
+          return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: .8,
+            ),
+            itemBuilder: (context, index) => Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, ProductInfo.id,
+                      arguments: products[index]);
+                },
+                child: Stack(
+                  children: <Widget>[
+                    Positioned.fill(
+                      child: Image(
+                        fit: BoxFit.fill,
+                        image: AssetImage(products[index].pLocation),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      child: Opacity(
+                        opacity: .6,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 60,
+                          color: Colors.white,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 5),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  products[index].pName,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text('\$ ${products[index].pPrice}')
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            itemCount: products.length,
+          );
+        } else {
+          return Center(child: Text('Loading...'));
+        }
+      },
+    );
+  }
+  Widget TshirttsView() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _store.loadProducts(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<Product> products = [];
+          for (var doc in snapshot.data.documents) {
+            var data = doc.data;
+            products.add(Product(
+                pId: doc.documentID,
+                pPrice: data[kProductPrice],
+                pName: data[kProductName],
+                pDescription: data[kProductDescription],
+                pLocation: data[kProductLocation],
+                pCategory: data[kProductCategory]));
+          }
+          _products = [...products];
+          products.clear();
+          products = getProductByCategory(kTshirts, _products);
+          return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: .8,
+            ),
+            itemBuilder: (context, index) => Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, ProductInfo.id,
+                      arguments: products[index]);
+                },
+                child: Stack(
+                  children: <Widget>[
+                    Positioned.fill(
+                      child: Image(
+                        fit: BoxFit.fill,
+                        image: AssetImage(products[index].pLocation),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      child: Opacity(
+                        opacity: .6,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 60,
+                          color: Colors.white,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 5),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  products[index].pName,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text('\$ ${products[index].pPrice}')
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            itemCount: products.length,
+          );
+        } else {
+          return Center(child: Text('Loading...'));
+        }
+      },
+    );
+  }
+
+}
